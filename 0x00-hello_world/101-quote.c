dd635fac7bf0ea2ebfeea1e9a38f055f1a5cf87c
Zillight <banjiolawolez@gmail.com>
2022-12-08 05:38:06 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
