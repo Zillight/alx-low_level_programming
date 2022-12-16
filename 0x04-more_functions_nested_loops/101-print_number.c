30f22670d640c68b1c36747ed31c3209ed9deef3
Zillight <banjiolawolez@gmail.com>
2022-12-15 19:57:45 -0800

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -21,3 +21,17 @@ void print_number(int n)
 	}
 	putchar(i % 10 + '0');
 }
+int main(void)
+{
+	print_number(98);
+	putchar('\n');
+	print_number(402);
+	putchar('\n');
+	print_number(1024);
+	putchar('\n');
+	print_number(0);
+	putchar('\n');
+	print_number(-98);
+	putchar('\n');
+	return (0);
+}
