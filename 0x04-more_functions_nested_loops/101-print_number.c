09c95da7d563427fdb47b131000ff10b0cedd140
Zillight <banjiolawolez@gmail.com>
2022-12-15 19:12:55 -0800

    New changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,26 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		n1 = -n;
-		putchar('-');
+		putchar(45);
+		m = n * -1;
 	} else
 	{
-		n1 = n;
+		m = n;
 	}
-
-	if (n1 / 10)
+	d = m;
+	count = 1;
+	while (d > 9)
 	{
-		print_number(n1 / 10);
+		d /= 10;
+		count *= 10;
 	}
 
-	putchar((n1 % 10) + '0');
+	for (; count >= 1; count /= 10)
+	{
+		putchar(((m / count) % 10) + 48);
+	}
 }
