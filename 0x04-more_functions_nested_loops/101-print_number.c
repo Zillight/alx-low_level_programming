343d35a8aceca9c521587768fe4d668fd2ec0cf9
Zillight <banjiolawolez@gmail.com>
2022-12-15 19:36:23 -0800

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,17 +5,18 @@
  * main - check the code
  * Return: Always 0.
  */
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
-	putchar('\n');
-	print_number(402);
-	putchar('\n');
-	print_number(1024);
-	putchar('\n');
-	print_number(0);
-	putchar('\n');
-	print_number(-98);
-	putchar('\n');
-	return (0);
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -1;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	putchar(i % 10 + '0');
 }
